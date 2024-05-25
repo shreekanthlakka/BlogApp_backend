958edd75d0c1c714c333662dc591fde38ff6ee5b
shreekanthlakka <shreekanth.lakka@hotmail.com>
2024-05-25 21:49:38 +0530

    added some cookie options

--- utils/sendCookies.js
+++ utils/sendCookies.js
@@ -17,6 +17,7 @@ const sendCookies = async (_id, res) => {
         const options = {
             httpOnly: true,
             secure: true,
+            sameSite: "None",
             maxAge: 1000 * 60 * 60 * 1,
         };
         res.status(200).cookie("accessToken", accessToken, options).json({
