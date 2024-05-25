84983f99908a19ab2cf49cad3f142f37d0bb70d8
shreekanthlakka <shreekanth.lakka@hotmail.com>
2024-05-25 19:55:13 +0530

    path join

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,8 +1,11 @@
 import multer from "multer";
+import path, { dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/temp");
+        cb(null, path.join(__dirname, "./public/temp"));
     },
     filename: function (req, file, cb) {
         cb(null, `${Math.floor(Math.random() * 90000)}#${file.originalname}`);
