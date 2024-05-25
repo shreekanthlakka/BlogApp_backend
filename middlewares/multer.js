8d1e1469186e8cc7e8a61a485e541ffcdda93838
shreekanthlakka <shreekanth.lakka@hotmail.com>
2024-05-25 20:14:52 +0530

    temp files added

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -6,7 +6,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, "./public/temp"));
+        cb(null, "./public/temp");
     },
     filename: function (req, file, cb) {
         cb(null, `${Math.floor(Math.random() * 90000)}#${file.originalname}`);
